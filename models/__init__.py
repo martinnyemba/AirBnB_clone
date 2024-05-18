8690228b7186be4b8a085a95ec3a36e9bf90ba57
Eng Nyemba Martin <martinnyemba@gmail.com>
2024-05-18 09:42:33 +0200

    Update models/__init__.py: created a unique FileStorage instance for the application

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+"""__init__ magic method for models directory
+Initializes the package
+"""
+# import file_storage.py
+from models.engine.file_storage import FileStorage
+
+# create the variable storage, an instance of FileStorage
+storage = FileStorage()
+# call reload() method
+storage.relaod()
